c280e113d7a978afa9ab96c35c4fb1df61c3200c
Kim dongwon <skqorwkdal@naver.com>
2021-12-25 11:54:30 +0900

    1 commit

---
+++ src/components/TestimonialsSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from './SectionTitle';
+
+export default function TestimonialsSection() {
+  return (
+    <div>
+      <div className="container">
+        <SectionTitle
+          heading="Testimonials"
+          subheading="see what out clients say about us"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServiceSection';
 import ProjectsSection from '../components/ProjectsSection';
+import ContactBanner from '../components/ContactBanner';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <AboutSection />
       <ServiceSection />
       <ProjectsSection />
+      <ContactBanner />
     </div>
   );
 }
